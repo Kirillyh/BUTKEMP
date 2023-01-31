01849048cddd60f684d520e5bf99b218529d26fb
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2023-01-31 18:27:28 +0300

    Сделали задачу найти сумму трех цифр

---
+++ Urok1/Program.cs
@@ -0,0 +1,26 @@
+﻿//array - 10
+//m - 3
+
+//1 5 4 1 3 4 7 4 1 2
+//m = 3
+//max-> 
+
+int m = 3;
+int size = 10;                        // Рандомнный массив
+int[] array = Enumerable.Range(1,size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+Console.WriteLine($"[{string.Join(' ' , array)}]");
+
+int max = 0;
+
+for (int i = 0; i < array.Length - m; i++)
+{
+    int t = 0;
+    for (int j = i; j < i + m ; j++)
+    {
+     t =t + array[j];        
+    }
+    if(t > max)max =t;
+}
+Console.WriteLine(max);
